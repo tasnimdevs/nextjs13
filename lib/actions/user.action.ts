b69328e5a9929a0d618b68f1a939805dfc9b528a
SaBiT495 <shahedbillah495@gmail.com>
2024-01-25 12:35:22 +0600

    for deploy webhook

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -38,6 +38,7 @@ export async function createUser(userData: CreateUserParams) {
     throw error;
   }
 }
+
 export async function updateUser(params: UpdateUserParams) {
   try {
     connectToDatabase();
