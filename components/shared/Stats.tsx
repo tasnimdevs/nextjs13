1432a36a813bc2aeb5c686dcf78ca7325a8b3720
SaBiT495 <shahedbillah495@gmail.com>
2024-03-10 16:45:19 +0600

    update state and reputation

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,4 +1,5 @@
 import { formatAndDivideNumber } from "@/lib/utils";
+import { BadgeCounts } from "@/types";
 import Image from "next/image";
 import React from "react";
 interface StatsCardProps {
@@ -9,7 +10,7 @@ interface StatsCardProps {
 
 const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
   return (
-    <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-start gap-4 rounded-md bordr p-6 shadow-light-300 dark:shadow-dark-200">
+    <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-start gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
       <Image src={imgUrl} alt="title" width={40} height={50} />
       <div className="paragraph-semibold text-dark200_light900">
         <p className="paragraph-semibold text-dark200_light900">{value}</p>
@@ -22,14 +23,18 @@ const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
 interface Props {
   totalQuestions: number;
   totalAnswers: number;
+  badges: BadgeCounts;
+  reputation: number;
 }
 
-const Stats = ({ totalQuestions, totalAnswers }: Props) => {
+const Stats = ({ totalQuestions, totalAnswers, badges, reputation }: Props) => {
   return (
     <div className="mt-10">
-      <h4 className="h3-semibold text-dark200_light900">Stats</h4>
+      <h4 className="h3-semibold text-dark200_light900">
+        Stats - {reputation}
+      </h4>
       <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
-        <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md bordr p-6 shadow-light-300 dark:shadow-dark-200">
+        <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
           <div>
             <p className="paragraph-semibold text-dark200_light900">
               {formatAndDivideNumber(totalQuestions)}
@@ -46,17 +51,17 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
 
         <StatsCard
           imgUrl="/assets/icons/gold-medal.svg"
-          value={0}
+          value={badges.GOLD}
           title="Gold Badges"
         />
         <StatsCard
           imgUrl="/assets/icons/silver-medal.svg"
-          value={0}
+          value={badges.SILVER}
           title="Silver Badges"
         />
         <StatsCard
           imgUrl="/assets/icons/bronze-medal.svg"
-          value={0}
+          value={badges.BRONZE}
           title="Bronze Badges"
         />
       </div>
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -18,6 +18,8 @@ import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
 import { FilterQuery } from "mongoose";
 import Answer from "@/database/answer.model";
+import { BadgeCriteriaType } from "@/types";
+import { assignBadges } from "../utils";
 
 export async function getUserById(params: any) {
   try {
@@ -246,69 +248,66 @@ export async function getUserInfo(params: GetUserByIdParams) {
 
     const user = await User.findOne({ clerkId: userId });
 
-    if (!user) {
-      throw new Error("User not found");
+    if(!user) {
+      throw new Error('User not found');
     }
 
-    const totalQuestions = await Question.countDocuments({ author: user._id });
+    const totalQuestions = await Question.countDocuments({ author: user._id })
     const totalAnswers = await Answer.countDocuments({ author: user._id });
 
-    const questionUpvotes = await Question.aggregate([
-      { $match: { autor: user._id } },
-      {
-        $project: {
-          _id: 0,
-          upvotes: { $size: "$upvotes" },
-        },
-      },
-      {
-        $group: {
-          _id: null,
-          totaleUvotes: { $sum: "$upvotes" },
-        },
-      },
-    ]);
-    const answerUpvotes = await Answer.aggregate([
-      { $match: { autor: user._id } },
-      {
-        $project: {
-          _id: 0,
-          upvotes: { $size: "$upvotes" },
-        },
-      },
-      {
-        $group: {
-          _id: null,
-          totaleUvotes: { $sum: "$upvotes" },
-        },
-      },
-    ]);
-    const questionViews = await Answer.aggregate([
-      { $match: { autor: user._id } },
-      {
-        $project: {
-          _id: 0,
-          upvotes: { $size: "$upvotes" },
-        },
-      },
-      {
-        $group: {
-          _id: null,
-          totaleUvotes: { $sum: "$upvotes" },
-        },
-      },
-    ]);
+    const [questionUpvotes] = await Question.aggregate([
+      { $match: { author: user._id }},
+      { $project: {
+        _id: 0, upvotes: { $size: "$upvotes" }
+      }},
+      { $group: {
+        _id: null,
+        totalUpvotes: { $sum: "$upvotes" }
+      }}
+    ])
+
+    const [answerUpvotes] = await Answer.aggregate([
+      { $match: { author: user._id }},
+      { $project: {
+        _id: 0, upvotes: { $size: "$upvotes" }
+      }},
+      { $group: {
+        _id: null,
+        totalUpvotes: { $sum: "$upvotes" }
+      }}
+    ])
+
+    const [questionViews] = await Answer.aggregate([
+      { $match: { author: user._id }},
+      { $group: {
+        _id: null,
+        totalViews: { $sum: "$views" }
+      }}
+    ])
+
+    const criteria = [
+      { type: 'QUESTION_COUNT' as BadgeCriteriaType, count: totalQuestions },
+      { type: 'ANSWER_COUNT' as BadgeCriteriaType, count: totalAnswers },
+      { type: 'QUESTION_UPVOTES' as BadgeCriteriaType, count: questionUpvotes?.totalUpvotes || 0 },
+      { type: 'ANSWER_UPVOTES' as BadgeCriteriaType, count: answerUpvotes?.totalUpvotes || 0 },
+      { type: 'TOTAL_VIEWS' as BadgeCriteriaType, count: questionViews?.totalViews || 0 },
+    ]
+
+    const badgeCounts = assignBadges({ criteria });
 
     return {
       user,
       totalQuestions,
       totalAnswers,
-    };
+      badgeCounts,
+      reputation: user.reputation,
+    }    
   } catch (error) {
     console.log(error);
     throw error;
   }
 }
+
 
 export async function getUserQuestions(params: GetUserStatsParams) {
   try {
