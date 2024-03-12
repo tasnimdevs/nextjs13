9e875a90db04354821ef2378c4cafba657e53397
SaBiT495 <shahedbillah495@gmail.com>
2024-03-12 15:53:17 +0600

    fix mongobd connect

--- lib/actions/mongoose.ts
+++ lib/actions/mongoose.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+let isConnected: boolean = false;
 
 export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
@@ -8,9 +9,9 @@ export const connectToDatabase = async () => {
     return console.log("MISSING MONGODB_URL");
   }
 
- /*  if (isConnected) {
+   if (isConnected) {
     return console.log("MongoDB is already connected");
-  } */
+  } 
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
